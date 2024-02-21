0971c7c1c9ce52a45c83d228c0886f6b9cb23115
Mehran Shoushtari Moghadam <119717933+mehranUL@users.noreply.github.com>
2024-02-20 23:33:05 -0600

    Update Sine_lfsr_lfsr_Chu.m

--- Trigonometric Functions/Sine_lfsr_lfsr_Chu.m
+++ Trigonometric Functions/Sine_lfsr_lfsr_Chu.m
@@ -8,8 +8,8 @@ else
 %N = 1024;
 
 
-[~,lfval] = LFSR_TrigonoSC([true false true true true false false false false false],N/2,N); %N=1024
-%[~,lfval] = LFSR_TrigonoSC([true false true false true false false false false],N/2,N); %N=512
+[~,lfval] = LFSR_TrigonoSC([true false true true true false false false false false],X,N); %N=1024
+%[~,lfval] = LFSR_TrigonoSC([true false true false true false false false false],X,N); %N=512
 lfval = lfval/N;
 
 
